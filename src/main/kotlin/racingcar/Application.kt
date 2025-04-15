561baefd83c1e288e06d7f00f644ba99bd04fd7e
woowahan-pjs <jason@woowahan.com>
2025-04-15 21:30:07 +0900

    feat: set up the project

---
+++ src/main/kotlin/racingcar/Application.kt
@@ -0,0 +1,5 @@
+package racingcar
+
+fun main() {
+    // TODO: Implement the program
+}
---
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -0,0 +1,38 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class ApplicationTest : NsTest() {
+    @Test
+    fun `feature test`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("pobi,woni", "1")
+                assertThat(output()).contains("pobi : -", "woni : ", "Winners : pobi")
+            },
+            MOVING_FORWARD,
+            STOP,
+        )
+    }
+
+    @Test
+    fun `exception test`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
+        }
+    }
+
+    override fun runMain() {
+        main()
+    }
+
+    companion object {
+        private const val MOVING_FORWARD: Int = 4
+        private const val STOP: Int = 3
+    }
+}
